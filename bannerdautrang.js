bdb5bdd68cd9bcba5fc0b5f37a029f5eda53aa95
banhtrangtron <33902443+banhtrangtron@users.noreply.github.com>
2017-12-05 16:26:02 +0700

    Add files via upload

---
+++ bannerdautrang.js
@@ -0,0 +1,123 @@
+//please contact me nguyendinhtho2151@gmail.com
+//website http://batdongsanhcm.com.vn or https://nguyendinhtho.net
+
+(function($) {
+    $.fn.dinhtho = function(options) {        
+        var defaults = {  
+	    	animation: 'fadeAndPop', //fade, fadeAndPop, none
+		    animationspeed: 300, //how fast animtions are
+		    closeonbackgroundclick: true, //if you click background will modal close?
+		    dismissmodalclass: 'close-dinhtho-modal' //the class of a button or element that will close an open modal
+    	}; 
+
+        var options = $.extend({}, defaults, options); 
+	
+        return this.each(function() {
+        	var modal = $(this),
+        		topMeasure  = parseInt(modal.css('top')),
+				topOffset = modal.height() + topMeasure,
+          		locked = false,
+				modalBG = $('.dinhtho-modal-bg');
+
+			if(modalBG.length == 0) {
+				modalBG = $('<div class="dinhtho-modal-bg" />').insertAfter(modal);
+			}		    
+			modal.bind('dinhtho:open', function () {
+			  modalBG.unbind('click.modalEvent');
+				$('.' + options.dismissmodalclass).unbind('click.modalEvent');
+				if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modal.css({'top': $(document).scrollTop()-topOffset, 'opacity' : 0, 'visibility' : 'visible'});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"top": $(document).scrollTop()+topMeasure + 'px',
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					}
+					if(options.animation == "fade") {
+						modal.css({'opacity' : 0, 'visibility' : 'visible', 'top': $(document).scrollTop()+topMeasure});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					} 
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'visible', 'top':$(document).scrollTop()+topMeasure});
+						modalBG.css({"display":"block"});	
+						unlockModal()				
+					}
+				}
+				modal.unbind('dinhtho:open');
+			}); 	
+
+			modal.bind('dinhtho:close', function () {
+			  if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"top":  $(document).scrollTop()-topOffset + 'px',
+							"opacity" : 0
+						}, options.animationspeed/2, function() {
+							modal.css({'top':topMeasure, 'opacity' : 1, 'visibility' : 'hidden'});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "fade") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"opacity" : 0
+						}, options.animationspeed, function() {
+							modal.css({'opacity' : 1, 'visibility' : 'hidden', 'top' : topMeasure});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'hidden', 'top' : topMeasure});
+						modalBG.css({'display' : 'none'});	
+					}		
+				}
+				modal.unbind('dinhtho:close');
+			});     
+   	
+    	modal.trigger('dinhtho:open')
+			
+			//Close Modal Listeners
+			var closeButton = $('.' + options.dismissmodalclass).bind('click.modalEvent', function () {
+			  modal.trigger('dinhtho:close')
+			});
+			
+			if(options.closeonbackgroundclick) {
+				modalBG.css({"cursor":"pointer"})
+				modalBG.bind('click.modalEvent', function () {
+				  modal.trigger('dinhtho:close')
+				});
+			}
+			$('body').keyup(function(e) {
+        		if(e.which===27){ modal.trigger('dinhtho:close'); } // 27 is the keycode for the Escape key
+			});
+			
+			function unlockModal() { 
+				locked = false;
+			}
+			function lockModal() {
+				locked = true;
+			}	
+			
+        });//each call
+    }//orbit plugin call
+})(jQuery);
+        
+$(window).load(function() {
+		if(document.cookie.indexOf("adf") == -1)
+		{
+			document.cookie = "adfpopunder1=adf";
+			var divpopup = document.createElement("div");
+			$(divpopup).attr('id', 'myModal');
+			$(divpopup).attr('class', 'dinhtho-modal');
+			$( "body" ).append(divpopup);
+			$("#myModal").html("<a href='https://goo.gl/rMRuJk' target='_blank'><img src='https://2.bp.blogspot.com/-Os3wOsXJWlQ/WiZhuVolgmI/AAAAAAAAASA/LybNd3tVXcESAOic5yyXfKxSzlQgbfbPQCLcBGAs/s1600/du-an-saigon-mystery-mobi.jpg' width='500px'/><br/>24% Khi tham quan dự án</a>. Tặng ngay Nội Thất Châu Âu <h2><a class='close-dinhtho-modal'>X</a></h2>");
+			$('#myModal').dinhtho($('#myModal').data());
+		}
+});
